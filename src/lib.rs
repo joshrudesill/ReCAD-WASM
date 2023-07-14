f9c8da1d7ed3987b34329062a150cf6083b11cb1
Joshua Rudesill <joshrudesill@gmail.com>
2023-07-14 19:28:41 +0200

    experimenting

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
+
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn add(left: usize, right: usize) -> usize {
     left + right
+}
+#[wasm_bindgen]
+pub fn derive_actual_pos(cursor: f32, stage: f32, inverse_z: f32 ) -> f32 {
+    (((cursor + stage) * inverse_z)  / 1000.0).round() * 1000.0
 }
