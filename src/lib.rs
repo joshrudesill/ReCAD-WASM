e8fc1a434a3579c260f27d28bcea72f50f4bbc11
Joshua Rudesill <joshrudesill@gmail.com>
2023-07-18 22:46:37 +0200

    trying out some data structures

--- src/lib.rs
+++ src/lib.rs
@@ -3,20 +3,96 @@
 use wasm_bindgen::prelude::*;
 use js_sys::Float32Array;
 use js_sys::Array;
-use js_sys::Reflect;
-
 
 pub struct XY_global {
-    x: f32,
-    y: f32
+    x:  f32,
+    y:  f32
 }
-#[wasm_bindgen]
+
 pub struct NormalizedBox {
     b_L: XY_global,
     b_R: XY_global,
     t_L: XY_global,
-    t_R: XY_global
+    t_R: XY_global,
+   
 }
+pub struct BoxSides {
+    b: (XY_global, XY_global),
+    l: (XY_global, XY_global),
+    t: (XY_global, XY_global),
+    r: (XY_global, XY_global)
+}
+impl<'a> BoxSides {
+    fn new (s: &'a NormalizedBox ) -> Self {
+        Self {
+            b: (s.b_L, s.b_R),
+            l: (s.b_L, s.b_R),
+            r: (s.b_L, s.b_R),
+            t: (s.b_L, s.b_R)
+        }
+    }
+}
+impl NormalizedBox {
+    fn new(sx:  f32, sy:  f32, ex:  f32, ey:  f32) -> NormalizedBox<> {
+        let mut pos_x: bool = false;
+        let mut pos_y: bool = false;
+
+        if sx < ex {
+            pos_x = true;
+        }
+        if sy < ey {
+            pos_y = true;
+        }
+
+        let b_L: XY_global = XY_global { 
+            x: if pos_x {
+                sx
+            } else { 
+                ex 
+            }, y: if pos_y {
+                sy
+            } else {
+                ey
+            }
+        };
+        let b_R: XY_global = XY_global { 
+            x: if pos_x {
+                ex
+            } else { 
+                sx 
+            }, y: if pos_y {
+                sy
+            } else {
+                ey
+            }
+        };
+        let t_L: XY_global = XY_global { 
+            x: if pos_x {
+                sx 
+            } else { 
+                ex
+            }, y: if pos_y {
+                ey
+            } else {
+                sy
+            }
+        };
+        let t_R: XY_global = XY_global { 
+            x: if pos_x {
+                ex
+            } else { 
+                ex 
+            }, y: if pos_y {
+                ey
+            } else {
+                sy
+            }
+        };
+
+        NormalizedBox { b_L, b_R, t_L, t_R}
+    }
+}
+
 #[wasm_bindgen]
 #[derive(Clone)]
 pub struct Geometry {
@@ -31,9 +107,6 @@ pub struct RealGeometry {
 }
 
 
-impl NormalizedBox {
-    fn new() {}
-}
 
 #[wasm_bindgen]
 pub fn derive_actual_pos(cursor: f32, stage: f32, inverse_z: f32 ) -> f32 {
@@ -80,3 +153,40 @@ pub fn check_geo_collision(sel_box: &Float32Array, real_geo: &Array) {
     
 }
 
+fn check_line_intersect(f_line: (XY_global, XY_global), s_line: (XY_global, XY_global)) -> bool {
+
+    let (s, e) = f_line;
+    let (fs, fe) = s_line;
+    let XY_global {x: a, y: b} = s;
+    let XY_global {x: c, y: d} = e;
+    let XY_global {x: p, y: q} = fs;
+    let XY_global {x: r, y: s} = fe;
+
+
+    let det = (c - a) * (s - q) - (r - p) * (d - b);
+    if det.abs() < f32::EPSILON {
+        false
+    } else {
+        let lambda = ((s - q) * (r - a) + (p - r) * (s - b)) / det;
+        let gamma = ((b - d) * (r - a) + (c - a) * (s - b)) / det;
+        0.0 < lambda && lambda < 1.0 && 0.0 < gamma && gamma < 1.0
+    }
+}
+
+
+#[wasm_bindgen]
+pub fn check_line_collision(bsx: f32, bsy: f32, bex: f32, bey: f32, gsx: f32, gsy: f32, gex: f32, gey: f32,) -> bool {
+    let NormalizedBox {
+        b_L, 
+        b_R, 
+        t_L,
+        t_R, 
+    } = NormalizedBox::new(bsx, bsy, bex, bey);
+    
+     
+
+    if gsx >= b_L.x && gsx <= b_R.x && gsy >= b_L.y && gsy <= t_L.y { true; } 
+    if gex >= b_L.x && gex <= b_R.x && gey >= b_L.y && gey <= t_L.y { true; } 
+         
+    false
+}
