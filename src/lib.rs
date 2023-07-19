00bd6da9e9b9e41e28b9e6f1c76cd7aa3aa9f70a
Joshua Rudesill <joshrudesill@gmail.com>
2023-07-19 12:40:41 +0200

    line collision fn done for now

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,24 @@
 use wasm_bindgen::prelude::*;
 use js_sys::Float32Array;
 use js_sys::Array;
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
 
+}
+
+macro_rules! console_log {
+    // Note that this is using the `log` function imported above during
+    // `bare_bones`
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+#[derive(Copy, Clone)]
 pub struct XY_global {
     x:  f32,
     y:  f32
 }
-
 pub struct NormalizedBox {
     b_L: XY_global,
     b_R: XY_global,
@@ -22,13 +34,13 @@ pub struct BoxSides {
     t: (XY_global, XY_global),
     r: (XY_global, XY_global)
 }
-impl<'a> BoxSides {
-    fn new (s: &'a NormalizedBox ) -> Self {
+impl BoxSides {
+    fn new (s: &NormalizedBox ) -> Self {
         Self {
             b: (s.b_L, s.b_R),
-            l: (s.b_L, s.b_R),
-            r: (s.b_L, s.b_R),
-            t: (s.b_L, s.b_R)
+            l: (s.b_L, s.t_R),
+            r: (s.b_R, s.t_R),
+            t: (s.t_L, s.t_R)
         }
     }
 }
@@ -89,7 +101,7 @@ impl NormalizedBox {
             }
         };
 
-        NormalizedBox { b_L, b_R, t_L, t_R}
+        NormalizedBox { b_L, b_R, t_L, t_R }
     }
 }
 
@@ -163,30 +175,39 @@ fn check_line_intersect(f_line: (XY_global, XY_global), s_line: (XY_global, XY_g
     let XY_global {x: r, y: s} = fe;
 
 
-    let det = (c - a) * (s - q) - (r - p) * (d - b);
+    let det: f32 = (c - a) * (s - q) - (r - p) * (d - b);
     if det.abs() < f32::EPSILON {
-        false
+        return false
     } else {
-        let lambda = ((s - q) * (r - a) + (p - r) * (s - b)) / det;
-        let gamma = ((b - d) * (r - a) + (c - a) * (s - b)) / det;
-        0.0 < lambda && lambda < 1.0 && 0.0 < gamma && gamma < 1.0
+        let lambda: f32 = ((s - q) * (r - a) + (p - r) * (s - b)) / det;
+        let gamma: f32 = ((b - d) * (r - a) + (c - a) * (s - b)) / det;
+        if 0.0 < lambda && lambda < 1.0 && 0.0 < gamma && gamma < 1.0 { return true; }
     }
+    return false
 }
 
 
 #[wasm_bindgen]
 pub fn check_line_collision(bsx: f32, bsy: f32, bex: f32, bey: f32, gsx: f32, gsy: f32, gex: f32, gey: f32,) -> bool {
+    let n_box = NormalizedBox::new(bsx, bsy, bex, bey);
     let NormalizedBox {
         b_L, 
         b_R, 
         t_L,
         t_R, 
-    } = NormalizedBox::new(bsx, bsy, bex, bey);
-    
-     
+    } = n_box;
 
-    if gsx >= b_L.x && gsx <= b_R.x && gsy >= b_L.y && gsy <= t_L.y { true; } 
-    if gex >= b_L.x && gex <= b_R.x && gey >= b_L.y && gey <= t_L.y { true; } 
-         
+    if gsx >= b_L.x && gsx <= b_R.x && gsy >= b_L.y && gsy <= t_L.y { return true; } 
+    
+    if gex >= b_L.x && gex <= b_R.x && gey >= b_L.y && gey <= t_L.y { return true; } 
+    
+    let box_sides: BoxSides = BoxSides::new(&n_box);
+    let b_side_arr: [(XY_global, XY_global); 4] = [box_sides.b, box_sides.r, box_sides.l, box_sides.t];
+    let line_to_check = ( XY_global{x: gsx, y: gsy }, XY_global{ x: gex, y: gey } );
+    
+    for side in b_side_arr {
+        if check_line_intersect(side, line_to_check) { return true; }
+    }
+
     false
 }
